b5efc4101e223f03154835d113a4546ccc4b89eb
Mirella Ibañez <mirella.alina@gmail.com>
2020-08-24 20:52:02 -0300

    Finish exercise one

---
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Animal.java
@@ -0,0 +1,17 @@
+package ar.com.ada.online.second.objectexerciseone;
+
+public class Animal {
+    String breed;
+    String color;
+    String name;
+
+    void move(){
+        System.out.println("El animal se esta moviendo");
+    }
+    void sleep(){
+        System.out.println("El animal esta durmiendo");
+    }
+    void eat(){
+        System.out.println("El animal esta comiendo");
+    }
+}
---
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Vehicle.java
@@ -0,0 +1,14 @@
+package ar.com.ada.online.second.objectexerciseone;
+
+public class Vehicle {
+    String brand;
+    Integer maxSpeed;
+    String color;
+
+    void moveOn(){
+        System.out.println("El vehiculo se esta moviendo");
+    }
+    void brake(){
+        System.out.println("El vehiculo esta frenado");
+    }
+}
